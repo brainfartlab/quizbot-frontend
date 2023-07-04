31f2a5da1bbdbbc93503e1bac9f018b5cd8349ae
Antoine Vandermeersch <avdmeers@gmail.com>
2023-07-04 16:27:35 +0200

    Revert "use primary url"
    This reverts commit eccffe5934a9efe9414fc6d81697692f73a094ef.

--- src/interop.js
+++ src/interop.js
@@ -17,7 +17,7 @@ export const onReady = ({ env, app }) => {
     domain: env.AUTH0_TENANT,
     clientID: env.AUTH0_CLIENT_ID,
     responseType: 'token',
-    redirectUri: `${env.DEPLOY_PRIME_URL}/callback`,
+    redirectUri: `${env.URL}/callback`,
   });
 
   if (app.ports && app.ports.auth0authorize) {
