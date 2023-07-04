eccffe5934a9efe9414fc6d81697692f73a094ef
Antoine Vandermeersch <avdmeers@gmail.com>
2023-07-04 16:25:43 +0200

    use primary url

--- src/interop.js
+++ src/interop.js
@@ -17,7 +17,7 @@ export const onReady = ({ env, app }) => {
     domain: env.AUTH0_TENANT,
     clientID: env.AUTH0_CLIENT_ID,
     responseType: 'token',
-    redirectUri: `${env.URL}/callback`,
+    redirectUri: `${env.DEPLOY_PRIME_URL}/callback`,
   });
 
   if (app.ports && app.ports.auth0authorize) {
